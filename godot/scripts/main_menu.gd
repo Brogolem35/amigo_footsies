51540b6924560068aadb8cddeac04021d15e7c20
Brogolem35 <brogolem35@protonmail.com>
2025-04-22 13:57:44 +0300

    Silence warning

--- godot/scripts/main_menu.gd
+++ godot/scripts/main_menu.gd
@@ -183,6 +183,6 @@ func _on_start_button_pressed() -> void:
 	if !SyncManager.started && SteamManager.lobby_members.size() == 2:
 		start_game()
 
-func _on_start_message(peer_id: int) -> void:
+func _on_start_message(_peer_id: int) -> void:
 	if !SyncManager.started && SteamManager.lobby_members.size() == 2:
 		start_game()
